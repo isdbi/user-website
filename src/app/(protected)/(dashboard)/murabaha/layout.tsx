0641a64be37fc30b8c285e0738bc3d545c762650
YacineMK <merroukiyacine06@gmail.com>
2025-05-10 20:59:54 +0100

    [feat]: remove some pages

--- src/app/(protected)/(dashboard)/murabaha/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function TrackTwoLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return <main>{children}</main>;
-}
--- src/app/(protected)/(dashboard)/musharakah/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function TrackThreeLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return <main>{children}</main>;
-}
